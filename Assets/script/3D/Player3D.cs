7864331f6203cdb2d4b74c1691df70d9792b15f4
Rikkaaaaaaaaaaa <904490307@qq.com>
2025-10-21 15:04:39 +0800

    设置角色

---
+++ Assets/script/3D/Player3D.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3D : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5.0f;
+
+    private CharacterController cc;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+        if (moveDirection != Vector3.zero)
+        {
+            anim.SetBool("Idle", false);
+            anim.SetBool("Walk", true);
+        }
+        else
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Walk", false);
+        }
+
+        cc.Move(moveSpeed * Time.deltaTime * moveDirection);
+    }
+}
